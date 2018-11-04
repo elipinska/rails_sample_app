26f408bfcc93af7e8eb96e555bd02b0d4ab68069
Ewa Lipinska <ewa.lipinska@hotmail.com>
2018-11-04 23:30:00 +0000

    Improve scss styling

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-<<<<<<< HEAD
 require 'minitest/reporters'
-=======
-require "minitest/reporters"
->>>>>>> master
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
